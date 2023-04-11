ce7bc415b9212633450588539300d48454899c96
1871513040@qq.com <1871513040@qq.com>
2023-04-11 10:07:22 +0800

    第6次提交

--- git_test.py
+++ git_test.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import warnings
-warnings.filterwarnings('ignore')
+
 
 data=[i for i in range(100)]
 
@@ -13,8 +12,5 @@ plt.show()
 print(s)
 print(data)
 
-d=s*0.5*np.pi
+d=s*np.pi
 print(s)
-
-plt.plot(d,c='r')
-plt.show()
