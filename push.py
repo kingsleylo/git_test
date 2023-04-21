056e8398348c30880e4086e622d3ee39d04bb32d
Jingluo <1871513040@qq.com>
2023-04-21 09:21:51 +0800

    第13次提交

---
+++ push.py
@@ -0,0 +1,11 @@
+'''
+-*- coding: utf-8 -*-
+@Author   : Jingluo
+@FILE     : push
+@Time     : 2023/4/21 9:21
+@Email    : Kingsleylott@gmail.com
+@Github   : 
+@IDE      : PyCharm
+@Desc     : This project was created at Hubei University of Technology
+'''
+print('this is A test')
